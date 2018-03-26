49957355acb1c2684179d0d07680bba9d79a7445
ydd1337 <33605464+ydd1337@users.noreply.github.com>
2018-03-26 20:55:51 +0300

    Update PersonServiceImpl.java

--- src/main/java/com/example/service/PersonServiceImpl.java
+++ src/main/java/com/example/service/PersonServiceImpl.java
@@ -5,6 +5,8 @@ import com.example.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,12 +15,13 @@ import java.util.List;
 public class PersonServiceImpl implements PersonService {
     @Autowired
     private PersonRepository personRepository;
-   /* public List<Person> getAll() {
+   public List<Person> getAll() {
         return personRepository.findAll();
-    }*/
-   public Page<Person> getAll(Pageable pageable){
-       return  personRepository.findAll(pageable);
-   }
+    }
+
+    public Page<Person> findPaginated(int page, int size){
+        return  personRepository.findAll(new PageRequest(page,size, Sort.unsorted()));
+    }
 
     public Person getById(long personID) {
         return personRepository.findById(personID).get();
