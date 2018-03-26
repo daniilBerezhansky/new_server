9628cd9d185ca246454be3a295162630ef9f8335
ydd1337 <33605464+ydd1337@users.noreply.github.com>
2018-03-26 21:03:51 +0300

    Update GymControl.java

--- src/main/java/com/example/controller/GymControl.java
+++ src/main/java/com/example/controller/GymControl.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
-
+import javax.xml.ws.http.HTTPException;
 import java.util.List;
 
 @RestController
